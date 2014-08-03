115a094680a1800cb4c5e6e7897a892f06e02c6f
Amiya Sahu <amiya053@gmail.com>
2014-08-03 12:01:00 +0530

    Changed to latest

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,16 +1,16 @@
 <?php
 
 /*
-	Plugin Name: Simple Social Sharing v2.0
+	Plugin Name: Q2A Social Sharing 
 	Plugin URI: http://www.question2answer.org/qa/31585
-	Plugin Description: Adds Simple Clickable Social Sharing Buttons Below Questions
+	Plugin Description: Adds Clickable Social Sharing Buttons Below Questions
 	Plugin Version: 1.3
 	Plugin Date: 2014-02-01
-	Plugin Author: Digitizor Media + Amiya Sahu 
+	Plugin Author: Amiya Sahu 
 	Plugin Author URI: http://amiyasahu.com
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.5
-	Plugin Update Check URI: 
+	Plugin Update Check URI: https://raw.githubusercontent.com/amiyasahu/q2a-social-share/master/qa-plugin.php
 */
 
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
@@ -22,14 +22,14 @@ define('SOCIAL_SHARE_PLUGIN_DIR', dirname(__FILE__));
 define('SOCIAL_SHARE_PLUGIN_DIR_NAME', basename(dirname(__FILE__)));
 
 
-require_once SOCIAL_SHARE_PLUGIN_DIR . '/qa-simple-social-sharing-utils.php' ;
-require_once SOCIAL_SHARE_PLUGIN_DIR.'/qa-simple-social-sharing-options.php';
+require_once SOCIAL_SHARE_PLUGIN_DIR . '/qa-social-share-utils.php' ;
+require_once SOCIAL_SHARE_PLUGIN_DIR.'/qa-social-share-options.php';
 
-qa_register_plugin_layer('qa-simple-social-sharing-layer.php', 'Simple Social Sharing Layer');
-qa_register_plugin_module('module', 'qa-simple-social-sharing-admin.php', 'qa_simple_social_sharing_admin', 'Simple Social Sharing Admin');
-qa_register_plugin_module('widget', 'qa-simple-social-sharing-widget.php', 'qa_simple_social_sharing_widget', 'Simple Social Sharing Widget');
-// qa_register_plugin_module('module', 'qa-simple-social-sharing-options.php', 'qa_sss_opt', 'Simple Social Sharing Widget options ');
-qa_register_plugin_phrases('qa-simple-social-sharing-lang-*.php', 'sss_lang');
+qa_register_plugin_layer('qa-social-share-layer.php', 'Social Sharing Layer');
+qa_register_plugin_module('module', 'qa-social-share-admin.php', 'qa_social_share_admin', 'Social Sharing Admin');
+qa_register_plugin_module('widget', 'qa-social-share-widget.php', 'qa_social_share_widget', 'Social Sharing Widget');
+// qa_register_plugin_module('module', 'qa-social-share-options.php', 'qa_sss_opt', 'Social Sharing Widget options ');
+qa_register_plugin_phrases('qa-social-share-lang-*.php', 'sss_lang');
 
 /*
 	Omit PHP closing tag to help avoid accidental output
