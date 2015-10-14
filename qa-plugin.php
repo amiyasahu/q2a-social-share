11ce27009909d44f9b8cc08e9436d5c5a2a4e0e8
Amiya Sahu <amiya053@gmail.com>
2015-10-14 21:47:01 +0530

    version update

--- qa-plugin.php
+++ qa-plugin.php
@@ -4,8 +4,8 @@
         Plugin Name: Q2A Social Sharing
         Plugin URI: https://github.com/amiyasahu/q2a-social-share/
         Plugin Description: Adds Clickable Social Sharing Buttons Below Questions
-        Plugin Version: 1.6.2
-        Plugin Date: 2015-09-22
+        Plugin Version: 1.6.3
+        Plugin Date: 2015-10-14
         Plugin Author: Amiya Sahu
         Plugin Author URI: http://amiyasahu.com
         Plugin License: GPLv2
